75ef50ac6c263c13ec459a1b7ce0f6cc7d569822
sanket behera <sanket.behera@cilalabs.com>
2023-03-29 11:20:03 +0530

    bar chart bug fixing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import chartData from './Wine-Data.json'
 import React, { useEffect } from 'react';
 import * as echarts from 'echarts';
-import { filterDataByAxis, getOption } from './utils.js'
+import { filterDataForScatterChart,filterDataForBarChart, getOption } from './utils.js'
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     const myScatterChart = echarts.init(document.getElementById('scatter'));
     const myBarChart = echarts.init(document.getElementById('bar'));
     // getting required filtered axis data
-    const scatterData = filterDataByAxis(chartData, 'Color intensity', 'Hue');
-    const barData = filterDataByAxis(chartData, 'Alcohol', 'Malic Acid');
+    const scatterData = filterDataForScatterChart(chartData, 'Color intensity', 'Hue');
+    const barData = filterDataForBarChart(chartData, 'Alcohol', 'Malic Acid');
     // getting Options to draw
     const scatteredOptions = getOption({ xAxis: scatterData.xAxis, yAxis: scatterData.yAxis, type: 'scatter' })
     const barOptions = getOption({ xAxis: barData.xAxis, yAxis: barData.yAxis, type: 'bar' })
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,34 @@
 // filtering the dataset for chart
-export const filterDataByAxis = (chartData, row, col) => {
+export const filterDataForScatterChart = (chartData, row, col) => {
     let res = {
         xAxis: [],
         yAxis: [],
     };
+    
     chartData.sort((a, b) => parseFloat(a[row]) - parseFloat(b[row]))
     res.xAxis = chartData.map((item) => item[row]);
     res.yAxis = chartData.map((item) => item[col]);
+
     return res;
 }
 
+export const filterDataForBarChart = (chartData, row, col) => {
+  let res = {
+    xAxis: [],
+    yAxis: [],
+  }
+
+  let rowArr = chartData.map(data => data[row])
+  res.xAxis = [...new Set(rowArr)].sort((a,b) => a - b)
+  
+  res.xAxis.forEach(data => {
+    let dataArr = chartData.filter((obj) => obj[row] === data )
+    let sum = dataArr.reduce((a, val) => a + val[col], 0)
+    res.yAxis.push(sum/dataArr.length)
+  })
+
+  return res;
+}
 // creating options to draw the chart
 export const getOption = ({xAxis, yAxis, type}) => {
     let option = {
